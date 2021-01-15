299192d16c599b90cb31e755292323ccab4fe931
Zoltán Nagy <abesto@abesto.net>
2021-01-15 00:39:10 +0000

    Build from 05c251e81b03633f1c8d5af490c9ee2fc0a3d96c

--- rktrl_lib.js
+++ rktrl_lib.js
@@ -780,16 +780,16 @@ async function init(input) {
         var ret = wasm.memory;
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper531 = function(arg0, arg1, arg2) {
-        var ret = makeMutClosure(arg0, arg1, 51, __wbg_adapter_20);
+    imports.wbg.__wbindgen_closure_wrapper661 = function(arg0, arg1, arg2) {
+        var ret = makeMutClosure(arg0, arg1, 135, __wbg_adapter_20);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper5152 = function(arg0, arg1, arg2) {
-        var ret = makeMutClosure(arg0, arg1, 4024, __wbg_adapter_23);
+    imports.wbg.__wbindgen_closure_wrapper5149 = function(arg0, arg1, arg2) {
+        var ret = makeMutClosure(arg0, arg1, 4029, __wbg_adapter_23);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper5154 = function(arg0, arg1, arg2) {
-        var ret = makeMutClosure(arg0, arg1, 4024, __wbg_adapter_26);
+    imports.wbg.__wbindgen_closure_wrapper5151 = function(arg0, arg1, arg2) {
+        var ret = makeMutClosure(arg0, arg1, 4029, __wbg_adapter_26);
         return addHeapObject(ret);
     };
 
